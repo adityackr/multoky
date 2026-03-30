b15d5d7463993fe3b84f3bd22df4bb0d98d67e78
adityackr <aditya09me@gmail.com>
2026-03-30 16:04:47 +0600

    feat: add user model and install backend dependencies including mongoose, axios, and bcryptjs

---
+++ src/model/user.model.ts
@@ -0,0 +1,72 @@
+import mongoose from 'mongoose';
+
+interface IUser {
+	id?: mongoose.Types.ObjectId;
+	name: string;
+	email: string;
+	password?: string;
+	image?: string;
+	phone?: string;
+	role: 'admin' | 'user' | 'vendor';
+
+	// for vendor
+	shopName?: string;
+	shopAddress?: string;
+	gstNumber?: string;
+	isApproved?: boolean;
+	verificationStatus: 'pending' | 'approved' | 'rejected';
+	requestedAt: Date;
+	approvedAt: Date;
+	rejectedReason?: string;
+
+	vendorProducts?: mongoose.Types.ObjectId[];
+	orders?: mongoose.Types.ObjectId[];
+
+	cart?: {
+		product: mongoose.Types.ObjectId;
+		quantity: number;
+	}[];
+
+	createdAt?: Date;
+	updatedAt?: Date;
+}
+
+const userSchema = new mongoose.Schema<IUser>(
+	{
+		name: { type: String, required: true },
+		email: { type: String, required: true, unique: true },
+		password: { type: String },
+		image: { type: String },
+		phone: { type: String },
+		role: { type: String, enum: ['admin', 'user', 'vendor'], default: 'user' },
+
+		// for vendor
+		shopName: { type: String },
+		shopAddress: { type: String },
+		gstNumber: { type: String },
+		isApproved: { type: Boolean, default: false },
+		verificationStatus: {
+			type: String,
+			enum: ['pending', 'approved', 'rejected'],
+			default: 'pending',
+		},
+		requestedAt: { type: Date },
+		approvedAt: { type: Date },
+		rejectedReason: { type: String },
+
+		vendorProducts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
+		orders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }],
+
+		cart: [
+			{
+				product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+				quantity: { type: Number, default: 1 },
+			},
+		],
+	},
+	{ timestamps: true },
+);
+
+const User = mongoose.models?.User || mongoose.model<IUser>('User', userSchema);
+
+export default User;
